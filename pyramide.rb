7e0c396ef19c1e7d2cf42bc89d2728f6f0bfe980
bertyn99 <bboulikou@gmail.com>
2018-06-29 22:16:14 +0200

    changement d'un put

--- pyramide.rb
+++ pyramide.rb
@@ -1,7 +1,7 @@
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 etage=gets.to_i
 while etage>25||etage<1
-    puts "erreur: Choisissez un nombre entre 25 et 1"
+    puts "Veuiller Choisir un nombre entre 25 et 1"
     etage=gets.to_i
 end
 puts "Voici la pyramide :"
