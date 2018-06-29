1b28843c3afe6641850ce588ad5f85c6e5d5b8b0
bertyn99 <bboulikou@gmail.com>
2018-06-29 22:15:10 +0200

    condition ajouté

--- pyramide.rb
+++ pyramide.rb
@@ -1,2 +1,16 @@
-puts "Entrez un nombre compris entre 1 ou 25 pour la pyramide"
-nombre=gets.to_i
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+etage=gets.to_i
+while etage>25||etage<1
+    puts "erreur: Choisissez un nombre entre 25 et 1"
+    etage=gets.to_i
+end
+puts "Voici la pyramide :"
+    x=1
+    etage.times do
+        x.times do
+            print"#"
+        end
+            puts" "
+             x+=1
+                end   
+       
